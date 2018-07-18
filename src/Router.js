411cdfe7e76902cc635b2ab3e9c8a2424c2b8555
Anton Kuznetsov <isnifer@gmail.com>
2018-07-18 14:00:16 +0300

    [1.4.0] Add stack route index, show/dismiss modal for web now work

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
-import createHistory from 'history/createBrowserHistory'
+import { ModalRoute } from 'react-router-modal'
+import createBrowserHistory from 'history/createBrowserHistory'
 import createMemoryHistory from 'history/createMemoryHistory'
 import { compile } from 'path-to-regexp'
 import { parse, stringify } from 'qs'
@@ -13,11 +14,13 @@ export function createNavigationEnabledStore() {}
 export default class TipsiRouter {
   constructor(initialRoute, routes, useMemoryHistory = false, defaultRouteConfig = {}) {
     this.defaultRouteConfig = defaultRouteConfig
+    this.basename = defaultRouteConfig.basename || '/'
     this.history = useMemoryHistory
       ? this.createMemoryHistory(initialRoute, routes)
-      : createHistory({ basename: defaultRouteConfig.basename || '/' })
+      : createBrowserHistory({ basename: this.basename })
     this.navigationProvider = this.createRouter(initialRoute, routes)
     this.routes = routes
+    this.stack = 0
   }
 
   /* eslint-disable class-methods-use-this */
@@ -29,18 +32,25 @@ export default class TipsiRouter {
   }
 
   createRouter(initialRoute, routes) {
-    const elements = Object.entries(routes).reduce((memo, [key, route]) => (
-      memo.concat(
-        <Route
+    const elements = Object.entries(routes).reduce((memo, [key, route]) => {
+      const RouteContainer = route.modal ? ModalRoute : Route
+      const RouteComponent = route.component
+
+      const render = props => (
+        <RouteComponent isFocused {...props} {...props.location.state} /> // eslint-disable-line
+      )
+
+      const renderProps = route.modal ? { component: render } : { render }
+
+      return memo.concat(
+        <RouteContainer
           key={key}
           exact={route.exact || route.path === '/'}
           path={route.path}
-          render={props => (
-            <route.component {...props} {...props.location.state} />
-          )}
+          {...renderProps}
         />
       )
-    ), [])
+    }, [])
 
     return (
       <Router history={this.history} {...this.defaultRouteConfig}>
@@ -90,35 +100,48 @@ export default class TipsiRouter {
 
   push(e, route, paramsOrOptions = {}) {
     this.callHistoryMethodWithArguments('push', e, route, paramsOrOptions)
+    this.stack += 1
   }
 
   pop(e) {
     if (e) {
       e.preventDefault()
     }
+
     this.history.goBack()
+    this.stack = this.stack === 0 ? 0 : this.stack - 1
   }
 
   popToTop(e) {
     if (e) {
       e.preventDefault()
     }
-    this.history.go(-this.history.index)
+
+    if (this.stack !== 0) {
+      this.history.go(-this.stack)
+    } else if (this.history.location.pathname !== this.basename) {
+      this.replace(e, { path: this.basename })
+    }
+
+    this.stack = 0
   }
 
   replace(e, route, paramsOrOptions = {}) {
     this.callHistoryMethodWithArguments('replace', e, route, paramsOrOptions)
   }
 
-  showModal(e) {
-    if (e) {
-      e.preventDefault()
-    }
+  showModal(e, route, paramsOrOptions = {}) {
+    this.callHistoryMethodWithArguments('push', e, route, paramsOrOptions)
   }
 
-  dismissModal(e) {
+  async dismissModal(e) {
     if (e) {
       e.preventDefault()
     }
+
+    this.history.goBack()
+
+    // Avoid incorrect push-transition after dismiss
+    await new Promise(resolve => setTimeout(resolve, 0))
   }
 }
